751bd184b453e602d3488ac13093a7cb854fdca3
EomTark <kubergl@naver.com>
2021-02-01 14:12:39 +0900

    first commit

---
+++ DBSQL/DBSQL.go
@@ -0,0 +1,151 @@
+package DBSQL
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+)
+
+type Sales struct {
+	SID         string `json:"sid"`
+	PRODUCTNAME string `json:"productname"`
+	EA          string `json:"ea"`
+	SALESDATE   string `json:"salesdate"`
+	CNAME       string `json:"cname"`
+}
+
+type WeekAmount struct {
+	DAY    string `json:"day"`
+	AMOUNT string `json:"amount`
+}
+
+type ProductsAmount struct {
+	PRODUCTNAME string `json:"productname"`
+	AMOUNT      string `json:"amount"`
+	EA          string `json:"ea"`
+	PRICE       string `json:"price"`
+}
+
+type CompanyInfo struct {
+	SID     string `json:"sid"`
+	CNAME   string `json:"cname"`
+	TEL     string `json:"tel"`
+	ADDRESS string `json:"address"`
+}
+
+func DBToString(rows *sql.Rows, length int, flag string) string {
+	var i int = 0
+	if flag == "COMPANY" {
+		values := make([]CompanyInfo, length)
+		for rows.Next() {
+			rows.Scan(&values[i].SID, &values[i].CNAME, &values[i].TEL, &values[i].ADDRESS)
+			i++
+		}
+		j, _ := json.Marshal(values)
+
+		return string(j)
+	} else if flag == "WeekAmount" {
+		values := make([]WeekAmount, length)
+		temps := make([]WeekAmount, length)
+
+		for days := 0; days < 7; days++ {
+			now := time.Now()
+			before := now.AddDate(0, 0, -6+days)
+			timebefore := fmt.Sprintf("%d-%02d-%02d", before.Year(), before.Month(), before.Day())
+			values[days].DAY = timebefore
+		}
+
+		for rows.Next() {
+			rows.Scan(&temps[i].DAY, &temps[i].AMOUNT)
+			i++
+		}
+
+		for _, temp := range temps {
+			if temp.DAY == "" {
+				continue
+			}
+			fmt.Println(temp)
+
+			for index, value := range values {
+				if temp.DAY == value.DAY {
+					values[index].AMOUNT = temp.AMOUNT
+				}
+			}
+		}
+		j, _ := json.Marshal(values)
+
+		return string(j)
+	}
+
+	return "없는 플레그 입니다."
+}
+
+func GetWeekAmount(db *sql.DB, cNameCode string) string {
+	fmt.Println("Get Week Amount")
+
+	getSql := fmt.Sprintf(`select eom.sales_date, sum(eom.amount) from (select sales.sales_date, sum(sales.ea::int) as ea, sales.code,
+		(select price from product where sales.code=product.code) * sum(sales.ea::int) as amount
+		from sales, product
+		where to_date(sales_date, 'YYYY-MM-DD') <= NOW() and to_date(sales_date, 'YYYY-MM-DD') > NOW() - interval '7 DAYS'
+		and sales.code = product.code
+		and product.c_sid = '%s'
+		GROUP BY sales.sales_date, sales.code ORDER BY sales_date) as eom group by eom.sales_date`, cNameCode)
+	rows, err := db.Query(getSql)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	text := DBToString(rows, 7, "WeekAmount")
+
+	return text
+}
+
+func GetWeekProductAmount(db *sql.DB) {
+	fmt.Println("eeee")
+}
+
+func GetMonthAmount(db *sql.DB) {
+	fmt.Println("zzzz")
+}
+
+func GetMonthProductAmount(db *sql.DB) {
+	fmt.Println("cccc")
+}
+
+func GetYearAmount(db *sql.DB) {
+	fmt.Println("bbbb")
+}
+
+func GetYearProductAmount(db *sql.DB) {
+	fmt.Println("gggg")
+}
+
+func GetOneMonth(db *sql.DB) {
+	fmt.Println("mmmm")
+}
+
+func GetThreeMonth(db *sql.DB) {
+	fmt.Println("nnnn")
+}
+
+func GetStartEnd(db *sql.DB) {
+	fmt.Println("bbbb")
+}
+
+func GetCompanyInfo(db *sql.DB) string {
+	fmt.Println("COMPANY info")
+	var companyCnt int
+	_ = db.QueryRow(`SELECT COUNT(*) FROM company`).Scan(&companyCnt)
+
+	sqlString := fmt.Sprint(`SELECT * FROM company`)
+	rows, err := db.Query(sqlString)
+	if err != nil {
+		return "Error in GetCompanyInfo GetInfo"
+	}
+	defer rows.Close()
+
+	text := DBToString(rows, companyCnt, "COMPANY")
+	return text
+}
---
+++ main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/eom/product-amount/DBSQL"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+const (
+	DB_USER     = "postgres"
+	DB_PASSWORD = "800326"
+	DB_NAME     = "postgres"
+)
+
+func getWeekAmount(c echo.Context) error {
+	defer c.Request().Body.Close()
+
+	cNameCode := c.Param("cname-code")
+	decodedValue, err := url.QueryUnescape(cNameCode)
+	if err != nil {
+		log.Fatal(err)
+	}
+	text := DBSQL.GetWeekAmount(db, strings.Split(decodedValue, " ")[1])
+	return c.String(http.StatusOK, text)
+}
+
+func getWeekProductAmount(c echo.Context) error {
+	defer c.Request().Body.Close()
+	DBSQL.GetWeekProductAmount(db)
+	return c.String(http.StatusOK, "return ok")
+}
+
+func getMonthAmount(c echo.Context) error {
+	defer c.Request().Body.Close()
+	DBSQL.GetMonthAmount(db)
+	return c.String(http.StatusOK, "return ok")
+}
+
+func getMonthProductAmount(c echo.Context) error {
+	defer c.Request().Body.Close()
+	DBSQL.GetMonthProductAmount(db)
+	return c.String(http.StatusOK, "return ok")
+}
+
+func getYearAmount(c echo.Context) error {
+	defer c.Request().Body.Close()
+	DBSQL.GetYearAmount(db)
+	return c.String(http.StatusOK, "return ok")
+}
+
+func getYearProductAmount(c echo.Context) error {
+	defer c.Request().Body.Close()
+	DBSQL.GetYearProductAmount(db)
+	return c.String(http.StatusOK, "return ok")
+}
+
+func getOneMonth(c echo.Context) error {
+	defer c.Request().Body.Close()
+	DBSQL.GetOneMonth(db)
+	return c.String(http.StatusOK, "return ok")
+}
+
+func getThreeMonth(c echo.Context) error {
+	defer c.Request().Body.Close()
+	DBSQL.GetThreeMonth(db)
+	return c.String(http.StatusOK, "return ok")
+}
+
+func getStartEnd(c echo.Context) error {
+	defer c.Request().Body.Close()
+	DBSQL.GetStartEnd(db)
+	return c.String(http.StatusOK, "return ok")
+}
+
+func getCompanyInfo(c echo.Context) error {
+	defer c.Request().Body.Close()
+	companyInfos := DBSQL.GetCompanyInfo(db)
+	return c.String(http.StatusOK, companyInfos)
+}
+
+func main() {
+	var err error
+
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
+	db, err = sql.Open("postgres", dbinfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	e := echo.New()
+
+	e.Use(middleware.CORS())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `[${time_rfc3339}] ${status} ${method} ${host} ${path} ${latency_human}` + "\n",
+	}))
+
+	e.GET("/GET/COMPANYID", getCompanyInfo)
+
+	groupCompany := e.Group("/SALES/COMPANY")
+	groupPeriod := e.Group("/SALES/PERIOD")
+
+	groupCompany.GET("/GET/WEEK/AMOUNT/:cname-code", getWeekAmount)
+	groupCompany.GET("/GET/WEEK/PRODUCTS-AMOUNT/:cname-code", getWeekProductAmount)
+
+	groupCompany.GET("/GET/MONTH/AMOUNT/:cname-code", getMonthAmount)
+	groupCompany.GET("/GET/MONTH/PRODUCTS-AMOUNT/:cname-code", getMonthProductAmount)
+
+	groupCompany.GET("/GET/YEAR/AMOUNT/:cname-code", getYearAmount)
+	groupCompany.GET("/GET/YEAR/PRODUCTS-AMOUNT/:cname-code", getYearProductAmount)
+
+	groupPeriod.GET("/GET/ONEMONTH", getOneMonth)
+	groupPeriod.GET("/GET/THREEMONTH", getThreeMonth)
+	groupPeriod.GET("/GET/START-END", getStartEnd)
+
+	e.Start(":8000")
+}
